4516aace3ae1b775622f2a8e703b6b6e0d87c254
javitoperal <jaaviperal@hotmail.com>
2018-06-27 23:50:52 -0300

    Fin

---
+++ main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+float sum(float a, float b) {
+
+	float ans = a+b;
+	printf("Your result is:%.3f\n", ans);
+
+	return 0;
+}
+
+float difference(float a, float b) {
+
+	float ans = a-b;
+	printf("Your result is:%.3f\n", ans);	
+
+	return 0;
+}
+
+float multi(float a, float b) {
+
+	float ans = a*b;
+	printf("Your result is:%.3f\n", ans);
+
+	return 0;
+}
+
+float div(float a, float b) {
+
+	int operations();
+	float ans = a/b;
+	if (b == 0) {
+		printf("You cant divide by 0\n");
+	
+		operations();
+
+	}
+	
+		
+	printf("Your result is:%.3f\n", ans);
+
+	return 0;
+}
+
+int operations() {
+
+
+	float an[1];
+	float a;
+	float b;
+	printf("What tha fak ya wanna do?\n");
+	printf("1)sum 2)difference 3)multiply 4)divide\n");
+	scanf("%f", &an[0]);
+	if (an[0] == 1){
+		
+		printf("Enter N1:");
+		scanf("%f", &a);
+		printf("Enter N2:");
+		scanf("%f", &b);
+
+		sum(a, b);
+		
+	}
+
+	if(an[0] == 2){
+		
+		printf("Enter N1:");
+		scanf("%f", &a);
+		printf("Enter N2:");
+		scanf("%f", &b);
+
+		difference(a, b);
+		
+	}
+
+	if(an[0] == 3){
+		
+		printf("Enter N1:");
+		scanf("%f", &a);
+		printf("Enter N2:");
+		scanf("%f", &b);
+
+		multi(a, b);
+		
+	}
+
+	if(an[0] == 4){
+		
+		printf("Enter N1:");
+		scanf("%f", &a);
+		printf("Enter N2:");
+		scanf("%f", &b);
+
+		div(a, b);
+		
+	}
+
+	return 0;
+}
+
+
+
+int main() {
+
+	operations();
+
+
+	return 0;
+}
